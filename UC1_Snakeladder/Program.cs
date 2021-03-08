89e01cde5631469e335bb8de5783bd6fdf18d65c
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-08 22:57:15 +0530

    [sarthak]ADD:snakeandladder

---
+++ UC1_Snakeladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC1_Snakeladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int POSITION = 0;
+            Console.WriteLine("player position is " +POSITION);
+        }
+    }
+}
