86cabfea137ea9af96d79e5f8460b8699ce3e57c
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-08 23:03:00 +0530

    [sarthak]ADD:snakeandladder

---
+++ UC2_Snakeandladder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC2_Snakeandladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine(" dice number is " +dice);
+        }
+    }
+}
