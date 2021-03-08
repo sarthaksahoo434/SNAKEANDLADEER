25286878f4d24dc727053246b7d2946a86a73be9
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-08 23:08:48 +0530

    [sarthak]ADD:snakeandladder

---
+++ UC3_Snakeandladder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UC3_Snakeandladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          const  int IS_NOPLAY = 1;
+           const int IS_LADDER = 2;
+           const int IS_SNAKE = 3;
+            int position = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            int option = random.Next(1, 4);
+            switch (option)
+            {
+                case IS_NOPLAY:
+                    position = 0;
+                    break;
+                case IS_SNAKE:
+                    position = position - dice;
+                    break;
+                case IS_LADDER:
+                    position = position + dice;
+                    break;
+            }
+            Console.WriteLine(" position is = " + position);
+        }
+    }
+}
